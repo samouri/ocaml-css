2055d2d2047e43b690055e606b2b1dc1cba7b5ff
Jake F - @samouri <samouri@users.noreply.github.com>
2018-09-06 12:09:13 -0400

    calling from javascript now works

--- js_export/Index.ml
+++ js_export/Index.ml
@@ -1,7 +1,7 @@
 let _ =
   Js.export_all
     (object%js
-      method parse = Lib.Index.parse
-      method print = Lib.Index.print
-      method astPrint = Lib.Index.astPrint
-    end)
+      method parse (s:(Js.js_string Js.t)) = Lib.Index.parse (Js.to_string s)
+      method print ast = Js.string (Lib.Index.print ast)
+      method astPrint ast = Js.string (Lib.Index.astPrint ast)
+    end);;
