1f03539a7b5acad61b19f009319b225681133a9b
Jake F - @samouri <samouri@users.noreply.github.com>
2018-10-04 18:16:12 -0400

    update website

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,9 +1,8 @@
-import { parse, print, astPrint } from './lib/css-parser';
+import { print, astPrint } from './lib/css-parser';
 import codemirror from './lib/codemirror';
 import './lib/code-mirror-modes/javascript';
 import './lib/code-mirror-modes/css';
 
-window.parse = parse;
-window.print = print;
+window.pprint = print;
 window.astPrint = astPrint;
 window.CodeMirror = codemirror;
