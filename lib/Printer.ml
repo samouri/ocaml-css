22ba7de50a2de5918a660f64eb610674d40a0588
Jake F - @samouri <samouri@users.noreply.github.com>
2018-12-31 16:20:40 -0700

    add automatic formatting

--- lib/Printer.ml
+++ lib/Printer.ml
@@ -1,168 +1,165 @@
 open Types
 
-let hasFractionalPart (num:float): bool = match modf num with 
-  | (0., _) -> false 
-  | _ -> true;;
+let hasFractionalPart (num : float) : bool =
+  match modf num with 0., _ -> false | _ -> true
 
 let printOpenerBlockType = function
   | Paren -> "("
   | SquareBracket -> "["
-  | Brace -> "{";;
+  | Brace -> "{"
 
 let printClosingBlockType = function
   | Paren -> ")"
   | SquareBracket -> "]"
-  | Brace -> "}";;
+  | Brace -> "}"
 
-type blockType = Paren | SquareBracket | Brace [@@deriving yojson, show];;
+type blockType = Paren | SquareBracket | Brace [@@deriving yojson, show]
 
-let rec print_cvalue ?(wrap=true) = function
+let rec print_cvalue ?(wrap = true) = function
   | String (ch, str) -> if wrap then Printf.sprintf "%c%s%c" ch str ch else str
   | Ident str -> str
   | Number i -> i
-  | Percentage i -> if hasFractionalPart i then (string_of_float i) ^ "%" else (string_of_int (int_of_float i)) ^ "%"
+  | Percentage i ->
+      if hasFractionalPart i then string_of_float i ^ "%"
+      else string_of_int (int_of_float i) ^ "%"
   | Uri str -> "url(" ^ str ^ ")"
-  | HexColor str -> str 
+  | HexColor str -> str
   | Delim d -> d
   | Dimension (num, str) -> Printf.sprintf "%f%s" num str
   | Func f -> f
   | UnicodeRange str -> str
   | Hash str -> "#" ^ str
-  | Block { token; value; pos } -> String.concat "" [
-    (printOpenerBlockType token);
-    (List.fold_left (fun a b -> (a ^ (print_cvalue b))) "" value);
-    (printClosingBlockType token);
-  ]
-  ;;
+  | Block {token; value; pos} ->
+      String.concat ""
+        [ printOpenerBlockType token
+        ; List.fold_left (fun a b -> a ^ print_cvalue b) "" value
+        ; printClosingBlockType token ]
 
 (*Gross hack here rn. Basically almost all cvalues get pprinted by space separators except for blocks which
   should not be separated. So if we are at last elem we know no space, and also if we know next elem isblock then no space
  *)
-let rec printCValueList ?(sep=" ") (expressions: component_value list) =
-  let isBlock  = function Block _ -> true | _ -> false in
+let rec printCValueList ?(sep = " ") (expressions : component_value list) =
+  let isBlock = function Block _ -> true | _ -> false in
   let isDelim = function Delim _ -> true | _ -> false in
-  let next i lst = List.nth lst (i+1) in
+  let next i lst = List.nth lst (i + 1) in
   expressions
-  |> List.mapi (fun (i:int) (cval:component_value) -> 
-    (print_cvalue cval) ^ 
-      if (i + 1 >= (List.length expressions)) || 
-         (isBlock (next i expressions)) ||
-         (isDelim (next i expressions)) ||
-         (isDelim cval)
-      then "" else sep)
+  |> List.mapi (fun (i : int) (cval : component_value) ->
+         print_cvalue cval
+         ^
+         if
+           i + 1 >= List.length expressions
+           || isBlock (next i expressions)
+           || isDelim (next i expressions)
+           || isDelim cval
+         then ""
+         else sep )
   |> String.concat ""
 
-let print_comment ({value}: comment) = "/*" ^ value ^ "*/";;
+let print_comment ({value} : comment) = "/*" ^ value ^ "*/"
 
-let print_declaration ({name; value; important;}: declaration) = 
-  Printf.sprintf "  %s: %s%s;" name (printCValueList value) (if important then " !important" else "");;
+let print_declaration ({name; value; important} : declaration) =
+  Printf.sprintf "  %s: %s%s;" name (printCValueList value)
+    (if important then " !important" else "")
 
-let print_declarations (rules: (declaration orComment) list) =
-  let printDeclarationOrComment (term:declaration orComment)= match term with 
-    | Comment c -> "  " ^ (print_comment c)
+let print_declarations (rules : declaration orComment list) =
+  let printDeclarationOrComment (term : declaration orComment) =
+    match term with
+    | Comment c -> "  " ^ print_comment c
     | Else declaration -> print_declaration declaration
   in
-  rules
-  |> List.map printDeclarationOrComment
-  |> String.concat "\n"
-;;
+  rules |> List.map printDeclarationOrComment |> String.concat "\n"
 
-let printAtRule {name; prelude; block;} = Printf.sprintf "@%s %s;" name (printCValueList prelude);;
+let printAtRule {name; prelude; block} =
+  Printf.sprintf "@%s %s;" name (printCValueList prelude)
 
-let rec printSelector = function 
+let rec printSelector = function
   | Simple selector -> printCValueList selector
   | Compound selector -> printCValueList ~sep:"" selector
-  | Complex selectors -> selectors |> List.map printSelector |> String.concat " "
-  ;;
+  | Complex selectors ->
+      selectors |> List.map printSelector |> String.concat " "
 
-let printStyleRule {prelude: Types.selector list; declarations;} = 
+let printStyleRule {prelude : Types.selector list; declarations} =
   let selectors = prelude |> List.map printSelector |> String.concat ",\n" in
   let declarationsS = print_declarations declarations in
-  Printf.sprintf "%s {\n%s\n}" selectors declarationsS;;
+  Printf.sprintf "%s {\n%s\n}" selectors declarationsS
 
-let printRule (rule: rule) = match rule with 
+let printRule (rule : rule) =
+  match rule with
   | Comment c -> print_comment c
   | AtRule r -> printAtRule r
   | StyleRule r -> printStyleRule r
-  ;;
 
-let optToStr = function None -> "" | Some s -> "" ^ s ^" ";;
+let optToStr = function None -> "" | Some s -> "" ^ s ^ " "
 
-let print_selectors selectors = String.concat ",\n" selectors;;
+let print_selectors selectors = String.concat ",\n" selectors
 
-let positionToJson (position:position): Yojson.Safe.json = match position with
-  (start, finish) -> `Assoc [
-    ("start", `Assoc [
-      ("line", `Int start.pos_lnum);
-      ("column", `Int (start.Lexing.pos_cnum - start.Lexing.pos_bol + 1))
-    ]);
-    ("end", `Assoc [
-      ("line", `Int finish.pos_lnum);
-      ("column", `Int (finish.Lexing.pos_cnum - finish.Lexing.pos_bol + 1))
-    ]);
-    ("source", `String start.pos_fname )
-  ]
+let positionToJson (position : position) : Yojson.Safe.json =
+  match position with start, finish ->
+    `Assoc
+      [ ( "start"
+        , `Assoc
+            [ ("line", `Int start.pos_lnum)
+            ; ( "column"
+              , `Int (start.Lexing.pos_cnum - start.Lexing.pos_bol + 1) ) ] )
+      ; ( "end"
+        , `Assoc
+            [ ("line", `Int finish.pos_lnum)
+            ; ( "column"
+              , `Int (finish.Lexing.pos_cnum - finish.Lexing.pos_bol + 1) ) ]
+        )
+      ; ("source", `String start.pos_fname) ]
 
-let termsToJson (value:component_value list) = match value with
-  _ -> `String (String.concat " " (List.map print_cvalue value))
-;;
+let termsToJson (value : component_value list) =
+  match value with _ ->
+    `String (String.concat " " (List.map print_cvalue value))
 
-let commentToJson ({value; pos}:comment) = 
-  `Assoc [ 
-     ("type", `String "comment");
-     ("comment", `String value );
-     ("position", positionToJson pos);
-    ]
- ;;
+let commentToJson ({value; pos} : comment) =
+  `Assoc
+    [ ("type", `String "comment")
+    ; ("comment", `String value)
+    ; ("position", positionToJson pos) ]
 
-let ruleToJson (rule:(declaration orComment)): Yojson.Safe.json = match rule with
-   | Comment c -> commentToJson c
-   | Else ({name; value; pos}) -> `Assoc [ 
-     ("type", `String "declaration");
-     ("property", `String name );
-     ("value", termsToJson value );
-     ("position", positionToJson pos);
-    ]
- ;;
+let ruleToJson (rule : declaration orComment) : Yojson.Safe.json =
+  match rule with
+  | Comment c -> commentToJson c
+  | Else {name; value; pos} ->
+      `Assoc
+        [ ("type", `String "declaration")
+        ; ("property", `String name)
+        ; ("value", termsToJson value)
+        ; ("position", positionToJson pos) ]
 
-let rulesetToJson (ruleset:rule): Yojson.Safe.json = match ruleset with
- | Comment c -> commentToJson c
- | AtRule ({name; prelude; block; pos;}) -> `Assoc [
-   ("type", `String name);
-   (name, `String(printCValueList prelude));
-   ("position", positionToJson pos); 
- ]
- | StyleRule {prelude; declarations; pos;} ->
-  `Assoc [
-   ("type", `String "rule");
-   ("selectors", `List (List.map (fun s -> `String (printSelector s)) prelude ));
-   ("declarations", `List (List.map ruleToJson declarations));
-   ("position", positionToJson pos);
-  ]
-;;
+let rulesetToJson (ruleset : rule) : Yojson.Safe.json =
+  match ruleset with
+  | Comment c -> commentToJson c
+  | AtRule {name; prelude; block; pos} ->
+      `Assoc
+        [ ("type", `String name)
+        ; (name, `String (printCValueList prelude))
+        ; ("position", positionToJson pos) ]
+  | StyleRule {prelude; declarations; pos} ->
+      `Assoc
+        [ ("type", `String "rule")
+        ; ( "selectors"
+          , `List (List.map (fun s -> `String (printSelector s)) prelude) )
+        ; ("declarations", `List (List.map ruleToJson declarations))
+        ; ("position", positionToJson pos) ]
 
-let rec rulesetsToJson (stylesheet:stylesheet): Yojson.Safe.json =  match stylesheet with 
- | [] -> `List []
- | (hd::tl) -> `List ((rulesetToJson hd) :: (List.map rulesetToJson tl))
- ;;
+let rec rulesetsToJson (stylesheet : stylesheet) : Yojson.Safe.json =
+  match stylesheet with
+  | [] -> `List []
+  | hd :: tl -> `List (rulesetToJson hd :: List.map rulesetToJson tl)
 
-let stylesheetToJson (stylesheet:stylesheet):Yojson.Safe.json =
-  `Assoc [
-    ("type", `String "stylesheet");
-    ("stylesheet", 
-      `Assoc [ (
-        "rules", (rulesetsToJson stylesheet)
-      )]
-    )
-  ]
-;;
+let stylesheetToJson (stylesheet : stylesheet) : Yojson.Safe.json =
+  `Assoc
+    [ ("type", `String "stylesheet")
+    ; ("stylesheet", `Assoc [("rules", rulesetsToJson stylesheet)]) ]
 
+let astPrint (rulesets : stylesheet) : string =
+  Yojson.Safe.pretty_to_string ~std:true (stylesheetToJson rulesets)
 
-let astPrint (rulesets:stylesheet): string = 
-  Yojson.Safe.pretty_to_string ~std:true (stylesheetToJson rulesets);;
-
-let prettyPrint (stylesheet:stylesheet) =
-  (stylesheet
-    |> List.map (fun ruleset -> printRule ruleset)
-    |> String.concat "\n\n") ^ "\n"
-;;
+let prettyPrint (stylesheet : stylesheet) =
+  ( stylesheet
+  |> List.map (fun ruleset -> printRule ruleset)
+  |> String.concat "\n\n" )
+  ^ "\n"
--- lib/Types.ml
+++ lib/Types.ml
@@ -1,25 +1,19 @@
-type operator = NoOp | Slash | Comma [@@deriving show, yojson];;
+type operator = NoOp | Slash | Comma [@@deriving show, yojson]
 
-type lexing_position = Lexing.position = {
-  pos_fname : string;
-  pos_lnum : int;
-  pos_bol : int;
-  pos_cnum : int;
-} [@@deriving yojson,show]
-;;
+type lexing_position = Lexing.position =
+  {pos_fname: string; pos_lnum: int; pos_bol: int; pos_cnum: int}
+[@@deriving yojson, show]
 
-type position = lexing_position * lexing_position [@@deriving yojson, show];;
+type position = lexing_position * lexing_position [@@deriving yojson, show]
 
-type blockType = Paren | SquareBracket | Brace [@@deriving yojson, show];;
-type block = { 
-  token: blockType; 
-  value: component_value list; 
-  pos: position; 
-} [@@deriving yojson, show]
+type blockType = Paren | SquareBracket | Brace [@@deriving yojson, show]
 
-and component_value = 
+type block = {token: blockType; value: component_value list; pos: position}
+[@@deriving yojson, show]
+
+and component_value =
   | Ident of string
-  | Func of string 
+  | Func of string
   | Hash of string
   | Number of string
   | Dimension of float * string
@@ -30,48 +24,42 @@ and component_value =
   | Uri of string
   | HexColor of string
   | Block of block
-  [@@deriving show, yojson];;
+[@@deriving show, yojson]
 
 (* need to differentiate between single and complex because the components of
   compound are not space separated *)
 type selector =
-  | Simple of component_value list (* Simple or Combinator *)
+  | Simple of component_value list
+  (* Simple or Combinator *)
   | Compound of component_value list
   | Complex of selector list
-  [@@deriving show, yojson];;
+[@@deriving show, yojson]
 
-type comment = { value: string; pos: position; } [@@deriving show, yojson];;
+type comment = {value: string; pos: position} [@@deriving show, yojson]
 
-type 'a orComment = Comment of comment | Else of 'a [@@deriving show, yojson];;
+type 'a orComment = Comment of comment | Else of 'a [@@deriving show, yojson]
 
-type atrule = {
-  name: string;
-  prelude: component_value list;
-  block: block option;
-  pos: position;
-} [@@deriving show, yojson];; 
+type atrule =
+  { name: string
+  ; prelude: component_value list
+  ; block: block option
+  ; pos: position }
+[@@deriving show, yojson]
 
-type declaration = {
-  name: string;
-  value: component_value list;
-  important: bool;
-  pos: position;
-} [@@deriving show, yojson];;
+type declaration =
+  {name: string; value: component_value list; important: bool; pos: position}
+[@@deriving show, yojson]
 
 (* type declarationOrComment = 
   | Comment of comment 
   | Declaration of declaration [@@deriving show, yojson];; *)
-type styleRule = { 
-  prelude: selector list;
-  declarations: (declaration orComment) list; 
-  pos: position;
-} [@@deriving show, yojson];;
+type styleRule =
+  { prelude: selector list
+  ; declarations: declaration orComment list
+  ; pos: position }
+[@@deriving show, yojson]
 
+type rule = Comment of comment | StyleRule of styleRule | AtRule of atrule
+[@@deriving show, yojson]
 
-type rule = 
-  | Comment of comment 
-  | StyleRule of styleRule
-  | AtRule of atrule
-  [@@deriving show, yojson];; 
-
-type stylesheet = (rule list) [@@deriving show, yojson];;
+type stylesheet = rule list [@@deriving show, yojson]
